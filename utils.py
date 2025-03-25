4582dbdddc3ffec3525c28ac4e21e74d96a9a82b
vlkuzun <vuzungil@gmail.com>
2025-03-25 15:56:22 +0000

    Add quick reading of software behaviour commands

---
+++ utils.py
@@ -0,0 +1,96 @@
+import os
+import json
+from dotmap import DotMap
+import pandas as pd
+from pathlib import Path
+from glob import glob
+from aeon.io.reader import Reader, Csv
+import aeon.io.api as api
+
+
+class SessionData(Reader):
+    """Extracts metadata information from a settings .jsonl file."""
+
+    def __init__(self, pattern="Metadata"):
+        super().__init__(pattern, columns=["metadata"], extension="jsonl")
+
+    def read(self, file):
+        """Returns metadata for the specified epoch."""
+        with open(file) as fp:
+            metadata = [json.loads(line) for line in fp] 
+
+        data = {
+            "metadata": [DotMap(entry['value']) for entry in metadata]
+        }
+        timestamps = [api.aeon(entry['seconds']) for entry in metadata]
+
+        return pd.DataFrame(data, index=timestamps, columns=self.columns)
+
+
+class Video(Csv):
+    """Extracts video frame metadata."""
+
+    def __init__(self, pattern="VideoData"):
+        super().__init__(pattern, columns=["hw_counter", "hw_timestamp", "_frame", "_path", "_epoch"])
+        self._rawcolumns = ["Time"] + self.columns[0:2]
+
+    def read(self, file):
+        """Reads video metadata from the specified file."""
+        data = pd.read_csv(file, header=0, names=self._rawcolumns)
+        data["_frame"] = data.index
+        data["_path"] = os.path.splitext(file)[0] + ".avi"
+        data["_epoch"] = file.parts[-3]
+        data["Time"] = data["Time"].transform(lambda x: api.aeon(x))
+        data.set_index("Time", inplace=True)
+        return data
+    
+
+class TimestampedCsvReader(Csv):
+    def __init__(self, pattern, columns):
+        super().__init__(pattern, columns, extension="csv")
+        self._rawcolumns = ["Time"] + columns
+
+    def read(self, file):
+        data = pd.read_csv(file, header=0, names=self._rawcolumns)
+        data["Seconds"] = data["Time"]
+        data["Time"] = data["Time"].transform(lambda x: api.aeon(x))
+        data.set_index("Time", inplace=True)
+        return data
+    
+
+def load_json(reader: SessionData, root: Path) -> pd.DataFrame:
+    root = Path(root)
+    pattern = f"{root.joinpath(root.name)}_*.{reader.extension}"
+    print(pattern)
+    data = [reader.read(Path(file)) for file in sorted(glob(pattern))]
+    return pd.concat(data)
+
+
+def load(reader: Reader, root: Path) -> pd.DataFrame:
+    root = Path(root)
+    pattern = f"{root.joinpath(root.name)}_{reader.register.address}_*.bin"
+    data = [reader.read(file) for file in sorted(glob(pattern))]
+    return pd.concat(data)
+
+
+def load_video(reader: Video, root: Path) -> pd.DataFrame:
+    root = Path(root)
+    pattern = f"{root.joinpath(root.name)}_*.csv"
+    data = [reader.read(Path(file)) for file in sorted(glob(pattern))]
+    return pd.concat(data)
+
+
+def concat_digi_events(series_low: pd.DataFrame, series_high: pd.DataFrame) -> pd.DataFrame:
+    """Concatenate seperate high and low dataframes to produce on/off vector"""
+    data_off = ~series_low[series_low==True]
+    data_on = series_high[series_high==True]
+    return pd.concat([data_off, data_on]).sort_index()
+
+
+def load_csv(reader: Csv, root: Path) -> pd.DataFrame:
+    root = Path(root)
+    pattern = f"{root.joinpath(reader.pattern).joinpath(reader.pattern)}_*.{reader.extension}"
+    print(pattern)
+    print([file for file in glob(pattern)])
+    data = pd.concat([reader.read(Path(file)) for file in glob(pattern)])
+    return data
