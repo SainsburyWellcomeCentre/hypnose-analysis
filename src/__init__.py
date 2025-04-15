2421aa1189bcaaa937589b1a3c535f41468489a1
vlkuzun <vuzungil@gmail.com>
2025-04-15 18:51:38 +0100

    Init changes

--- src/__init__.py
+++ src/__init__.py
@@ -1,9 +1,9 @@
 """
-Hypnose Processing - Tools for processing neurophysiology and behavioral data
+Hypnose Analysis - Tools for processing and analysing hypnose behavioral data
 """
 
 # Import the most commonly used components for easy access
 from .io import detect_stages
-from visualisation import plot_accuracy_single_subject, plot_rewards_single_subject, plot_rewards_multiple_subjects
+from .visualisation import plot_accuracy_single_subject, plot_rewards_single_subject, plot_rewards_multiple_subjects
 
 __version__ = '0.1.0'
