22d10a52895aaac9099c86da1a95f201801996f0
Joschua21 <joschua.geuter.22@ucl.ac.uk>
2025-09-29 11:31:08 +0100

    removed unnecessary files in the repo. Nothing else should've changed

--- src/__init__.py
+++ src/__init__.py
@@ -4,6 +4,5 @@ Hypnose Analysis - Tools for processing and analysing hypnose behavioral data
 
 # Import the most commonly used components for easy access
 from .processing import detect_subject_stages
-from .visualisation import plot_accuracy_single_subject, plot_rewards_single_subject, plot_rewards_multiple_subjects
 
 __version__ = '0.1.0'
