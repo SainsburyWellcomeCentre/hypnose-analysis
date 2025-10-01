373471af687d17352ab19a754db5e45885cd585e
Joschua21 <joschua.geuter.22@ucl.ac.uk>
2025-10-01 11:03:32 +0100

    small changed

--- src/__init__.py
+++ src/__init__.py
@@ -3,6 +3,5 @@ Hypnose Analysis - Tools for processing and analysing hypnose behavioral data
 """
 
 # Import the most commonly used components for easy access
-from .processing import detect_subject_stages
 
 __version__ = '0.1.0'
