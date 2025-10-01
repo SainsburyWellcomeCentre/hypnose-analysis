cc9c5083e5bc3bc8bc71f6bcfca474e016e50d26
vlkuzun <vuzungil@gmail.com>
2025-10-01 10:11:10 +0100

    Remove reference to non-existent script in __init__.py

--- src/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Hypnose Analysis - Tools for processing and analysing hypnose behavioral data
-"""
-
-# Import the most commonly used components for easy access
-from .processing import detect_subject_stages
-
-__version__ = '0.1.0'
